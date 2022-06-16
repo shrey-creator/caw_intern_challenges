9d3b8ad501b6e01b87cf1fe829e3626dc6e7a532
shrey-creator <shrey.agrawal003@gmail.com>
2022-06-16 11:24:49 +0530

    converted functions to arrow functions

--- keyboard/app.js
+++ keyboard/app.js
@@ -1,16 +1,13 @@
-
-function playAudio(index){
-    return function()
-    {
-    var audio = new Audio(`./audio/key-${index+1}.mp3`);
+const playAudio = (index) => {
+  return () => {
+    var audio = new Audio(`./audio/key-${index + 1}.mp3`);
     audio.play();
-    }
-}
-function addAudioToKeyboard()
-{
-    var keyboardBtns=document.querySelectorAll('a');
-    keyboardBtns.forEach((btn,index)=>{
-        btn.addEventListener("click",playAudio(index));
-    })
-}
-addAudioToKeyboard()
+  };
+};
+const addAudioToKeyboard = () => {
+  var keyboardBtns = document.querySelectorAll("a");
+  keyboardBtns.forEach((btn, index) => {
+    btn.addEventListener("click", playAudio(index));
+  });
+};
+addAudioToKeyboard();
